41f9011f822a62a7ae588ee40b81075701e53e01
Upol <upolabdurasulova312@gmail.com>
2024-06-15 20:10:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentFloor, setCurrentFloor] = useState(-1);
+  const [targetFloor, setTargetFloor] = useState(0);
+
+  const handleUp = () => {
+    if (currentFloor < 12) setCurrentFloor(currentFloor + 1);
+  };
+
+  const handleDown = () => {
+    if (currentFloor > -1) setCurrentFloor(currentFloor - 1);
+  };
+
+  const handleFloorChange = (event) => {
+    setTargetFloor(Number(event.target.value));
+  };
+
+  const moveToFloor = () => {
+    if (targetFloor >= -1 && targetFloor <= 12) {
+      setCurrentFloor(targetFloor);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="lift-container">
+        <div
+          className="lift"
+          style={{ bottom: `${((currentFloor + 1) / 14) * 100}% ` }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>{currentFloor}</p>
+        </div>
+      </div>
+      <div className="controls">
+        <button className="control-button" onClick={handleUp}>
+          Up
+        </button>
+        <button className="control-button" onClick={handleDown}>
+          Down
+        </button>
+        <input
+          className="floor-input"
+          type="number"
+          value={targetFloor}
+          onChange={handleFloorChange}
+          min="-1"
+          max="12"
+        />
+        <button className="control-button" onClick={moveToFloor}>
+          Go
+        </button>
+      </div>
     </div>
   );
 }
